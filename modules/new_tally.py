59c2a2c78609ec0e404add164eb8706cbbee5ff4
lmarchisio <51975294+lmarchisio@users.noreply.github.com>
2020-01-06 10:15:56 -0800

    Update new_tally.py

--- modules/new_tally.py
+++ modules/new_tally.py
@@ -9,39 +9,17 @@ def new_tally(project_name, token):
     # log all calls
     logging.basicConfig(filename='rwsheet.log', level=logging.INFO)
 
-    # create new IMS sheet in project management workspace
-    sheet_spec = ss_client.models.Sheet({
-        'name': project_name + ' Overview',
-        'columns': [{
-            'title': 'Project Name',
-            'primary': True,
-            'type': 'TEXT_NUMBER',
-            }, {
-            'title': 'Resource',
-            'type': 'PICKLIST',
-            'options': [
-                'CNC',
-                'Design',
-                'Fab',
-                'Install',
-                'Metal',
-                'Paint',
-                'Sculpt',
-                'Shipping',]
-            }, {
-            'title': 'Start',
-            'type': 'DATE',
-            }, {
-            'title': 'Finish',
-            'type': 'DATE',
-            }
-        ]
-    })
-
-    response = ss_client.Workspaces.create_sheet_in_workspace(
-        2148322879268740, #ID of IMS workspace
-        sheet_spec)
-    new_sheet = response.result
+    # copy overview template
+    response = ss_client.Sheets.copy_sheet(
+        6257571175655300,
+        smartsheet.models.ContainerDestination({
+            'destination_type': 'workspace',
+            'destination_id': 2148322879268740,
+            'new_name': project_name + ' Overview'
+        }),
+        include=['data']
+    )
 
     print('done')
                 
+
